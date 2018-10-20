784537278358bdd8c98f31289455daf50dd4f04b
odedia <odedia@gmail.com>
2018-10-20 11:08:35 +0300

    Update run-pipeline.sh

--- ci/run-pipeline.sh
+++ ci/run-pipeline.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -o allexport
 
-echo "Welcome to GCP on PKS pipeline! Here's your security public service announcement:"
-echo ""
-echo "DO NOT place variables.txt file inside the git repo directory."
-echo "DO NOT place the GCP Service Account Key JSON inside the git repo directory."
-echo ""
-
 SECRETS_FILE=$1
 if [ "${SECRETS_FILE}" == "" ]; then
   echo "Using default variables location at $HOME/secrets/variables.txt"
